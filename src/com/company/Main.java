125c89e0c69ee5e290d9c5e7eaeab5aa36008e40
Darkere <Dark-277@web.de>
2019-06-01 17:42:01 +0200

    remove debug

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(args.length);
         if(args.length  == 1) {
             UUID id = UUID.fromString(args[0]);
             System.out.println("Least: " + id.getLeastSignificantBits());
