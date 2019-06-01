0860835c01bce9cbb96fa12b2eeabf74bd133ddb
Darkere <Dark-277@web.de>
2019-06-01 18:44:41 +0200

    fix packages

--- src/mainpackage/UUIDTool.java
+++ src/mainpackage/UUIDTool.java
@@ -1,23 +1,23 @@
-package com.company;
+package mainpackage;
 
 import java.util.UUID;
 
 public class UUIDTool {
 
     public static void main(String[] args) {
-        if(args.length == 0) {
+        if (args.length == 0) {
             System.out.println("NEEDS ARGUMENTS");
         }
-        if(args.length  == 1) {
+        if (args.length == 1) {
             UUID id = UUID.fromString(args[0]);
             System.out.println("Least: " + id.getLeastSignificantBits());
             System.out.println("Most: " + id.getMostSignificantBits());
         }
-        if(args.length  == 2) {
-            UUID id = new UUID(Long.valueOf(args[1]),Long.valueOf(args[0]));
-            System.out.println("UUID: "+ id.toString());
+        if (args.length == 2) {
+            UUID id = new UUID(Long.valueOf(args[1]), Long.valueOf(args[0]));
+            System.out.println("UUID: " + id.toString());
         }
-        if(args.length > 2) {
+        if (args.length > 2) {
             System.out.println("TOO MANY ARGUMENTS");
         }
     }
